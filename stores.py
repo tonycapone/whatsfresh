42fe138b4b3fd1f97531d2608525dd1ebbf02dd6
Tony H <anthony.r.howell@gmail.com>
2014-04-19 22:58:20 -0400

    added adlinks to stores.py

--- stores.py
+++ stores.py
@@ -1,29 +1,34 @@
 
 stores = {'aldi': {
             'name': 'Aldi',
-            'intro': 'Aldi deals',
-            'departments': ['Meat', 'Produce'],
+            'intro': 'From this week\'s Aldi ad',
+            'departments': ['Meat', 'Produce', 'Dairy'],
+            'adlink': "http://weeklyads.aldi.us/aldi/default.aspx?action=entry&pretailerid=-97994&siteid=1337&mode=html&StoreID=2624123",
             },
         
         'schnucks': {
             'name': 'Schnucks',
-            'intro': 'Schnucks deals',
+            'intro': 'From this week\'s Schnucks ad',
             'departments': ['Meat', 'Produce', 'Dairy'],
+            'adlink': "http://schnucks.shoptocook.com/weeklyad.jsp?circularstoreidentifier=WHL1KQNZBR3ML1AKR10JYOVFWKCOFC6YBBJPMD",
             },
         'shopnsave': {
             'name': 'ShopNSave',
-            'intro': 'ShopNSave deals',
+            'intro': 'From this week\'s ShopNSave ad.',
             'departments': ['Meat', 'Produce','Dairy'],
+            'adlink': "http://shopnsave.com/savings/view-ads/search-circular.html?keyword=&circularId=93728&sneakPeek=false&storeId=4542",
             },
         'wholefoods': {
             'name': 'WholeFoods',
             'intro': "This week's deals at Whole Foods",
             'departments': [''],
+            'adlink': "http://www.wholefoodsmarket.com/sales-flyer?store=85"
             },
         'savealot': {
             'name': 'Save A Lot',
-            'intro': 'Save a Lot deals',
-            'departments': ['Meat', 'Produce'],
+            'intro': 'From this week\'s Save A Lot ad.',
+            'departments': ['Meat', 'Produce', 'Dairy'],
+            'adlink': "http://savealot.shoplocal.com/SaveALot/Entry/LandingContent?storeid=2655389"
             },
         
         }
