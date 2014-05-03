aa516659f22a47f62733c59433657ce4849baf55
Tony H <anthony.r.howell@gmail.com>
2014-05-03 10:35:52 -0400

    updated store intros

--- stores.py
+++ stores.py
@@ -1,38 +1,38 @@
 
 stores = {'aldi': {
             'name': 'Aldi',
-            'intro': 'From this week\'s Aldi ad',
+            'intro': 'Here\'s what\'s fresh this week at St. Louis area Aldi stores.' ,
             'departments': ['Meat', 'Produce', 'Dairy'],
             'adlink': "http://weeklyads.aldi.us/aldi/default.aspx?action=entry&pretailerid=-97994&siteid=1337&mode=html&StoreID=2624123",
             },
         
         'schnucks': {
             'name': 'Schnucks',
-            'intro': 'From this week\'s Schnucks ad',
+            'intro': 'Here\'s what\'s fresh this week at St. Louis area Schnucks stores.',
             'departments': ['Meat', 'Produce', 'Dairy'],
             'adlink': "http://schnucks.shoptocook.com/weeklyad.jsp?circularstoreidentifier=WHL1KQNZBR3ML1AKR10JYOVFWKCOFC6YBBJPMD",
             },
         'shopnsave': {
             'name': 'ShopNSave',
-            'intro': 'From this week\'s ShopNSave ad.',
+            'intro': 'Here\'s what\'s fresh this week at St. Louis area Shop n Save stores.',
             'departments': ['Meat', 'Produce','Dairy'],
             'adlink': "http://shopnsave.com/savings/view-ads/search-circular.html?keyword=&circularId=93728&sneakPeek=false&storeId=4542",
             },
         'wholefoods': {
             'name': 'WholeFoods',
-            'intro': "This week's deals at Whole Foods",
+            'intro': 'Here\'s what\'s fresh this week at St. Louis area Whole Foods Stores.',
             'departments': [''],
             'adlink': "http://www.wholefoodsmarket.com/sales-flyer?store=85"
             },
         'traderjoes': {
-            'name': 'Trader Joes',
+            'name': 'Here\'s what\'s fresh this week at St. Louis area Trader Joes stores.',
             'intro': "This week's deals at Trader Joes",
             'departments': [''],
             'adlink': "http://stlouis.findnsave.com/store/Trader-Joes/10272/"
-        }
+        },
         'savealot': {
             'name': 'Save A Lot',
-            'intro': 'From this week\'s Save A Lot ad.',
+            'intro': 'Here\'s what\'s fresh this week at St. Louis area Save A Lot stores.',
             'departments': ['Meat', 'Produce', 'Dairy'],
             'adlink': "http://savealot.shoplocal.com/SaveALot/Entry/LandingContent?storeid=2655389"
             },
